4a1dc689ded3b82fbfa85b75e87fced8651cb3e4
Christopher Young <cyoung@gmail.com>
2017-03-17 00:30:27 -0400

    Generalize METAR functions. Add PIREP lookup functions.

--- ADDS.go
+++ ADDS.go
@@ -2,6 +2,11 @@ package ADDS
 
 import (
 	"encoding/xml"
+	"fmt"
+	"github.com/kellydunn/golang-geo"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -23,9 +28,56 @@ func (t *ADDSTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 
 type ADDSData struct {
 	METARs []ADDSMETAR `xml:"METAR"`
+	PIREPs []ADDSPIREP `xml:"AircraftReport"`
 }
 
 type ADDSResponse struct {
 	RequestIndex int      `xml:"request_index"`
 	Data         ADDSData `xml:"data"`
+}
+
+func GetADDSData(url string) (ADDSData, error) {
+	fmt.Printf("URL: %s\n", url)
+	var ret ADDSResponse
+	resp, err := http.Get(url)
+	if err != nil || !strings.HasPrefix(resp.Status, "200") {
+		return ret.Data, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ret.Data, err
+	}
+
+	// Parse 'body'.
+	err = xml.Unmarshal([]byte(body), &ret)
+
+	//FIXME: Parse the error code and "warning" text that is sent.
+
+	return ret.Data, nil
+}
+
+var reportFlags = map[string]string{
+	"metars":          "&mostRecentForEachStation=constraint",
+	"aircraftreports": "",
+}
+
+func urlADDSDataByIdent(dataSource string, ident string) string {
+	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=%s&requestType=retrieve&format=xml&stationString=%s&hoursBeforeNow=1.5%s", dataSource, ident, reportFlags[dataSource])
+	return url
+}
+
+func urlLatestADDSDataInRect(dataSource string, bottomLeft, topRight *geo.Point) string {
+	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=%s&requestType=retrieve&format=xml&hoursBeforeNow=1.25&minLat=%f&minLon=%f&maxLat=%f&maxLon=%f%s", dataSource, bottomLeft.Lat(), bottomLeft.Lng(), topRight.Lat(), topRight.Lng(), reportFlags[dataSource])
+	return url
+}
+
+func urlLatestADDSDataInRadiusOf(dataSource string, radius uint, pt *geo.Point) string {
+	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=%s&requestType=retrieve&format=xml&hoursBeforeNow=1.25&radialDistance=%d;%f,%f%s", dataSource, radius, pt.Lng(), pt.Lat(), reportFlags[dataSource])
+	return url
+}
+
+func urlLatestADDSDataAlongRoute(dataSource string, dist float64, route string) string {
+	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=%s&requestType=retrieve&format=xml&hoursBeforeNow=1.25&flightPath=%f;%s%s", dataSource, dist, route, reportFlags[dataSource])
+	return url
 }
--- METAR.go
+++ METAR.go
@@ -1,13 +1,7 @@
 package ADDS
 
 import (
-	"encoding/xml"
-	"errors"
-	"fmt"
 	"github.com/kellydunn/golang-geo"
-	"io/ioutil"
-	"net/http"
-	"strings"
 )
 
 type ADDSMETAR struct {
@@ -26,49 +20,28 @@ type ADDSMETAR struct {
 }
 
 func GetADDSMETARs(url string) ([]ADDSMETAR, error) {
-	var ret ADDSResponse
-	resp, err := http.Get(url)
-	if err != nil || !strings.HasPrefix(resp.Status, "200") {
-		return ret.Data.METARs, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return ret.Data.METARs, err
-	}
-
-	// Parse 'body'.
-	err = xml.Unmarshal([]byte(body), &ret)
-
-	// Empty response? Typically happens with invalid identifiers.
-	if len(ret.Data.METARs) == 0 {
-		return ret.Data.METARs, errors.New("No results.")
-	}
-	return ret.Data.METARs, nil
+	r, err := GetADDSData(url)
+	return r.METARs, err
 }
 
 func GetADDSMETARsByIdent(ident string) ([]ADDSMETAR, error) {
-	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=%s&hoursBeforeNow=1.5", ident)
-	return GetADDSMETARs(url)
+	return GetADDSMETARs(urlADDSDataByIdent("metars", ident))
 }
 
 // Gets the most recent METARs that were obtained at least within the last 1.5 hours within a (lat, lng) defined rectangle.
 func GetLatestADDSMETARsInRect(bottomLeft, topRight *geo.Point) ([]ADDSMETAR, error) {
-	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&minLat=%f&minLon=%f&maxLat=%f&maxLon=%f", bottomLeft.Lat(), bottomLeft.Lng(), topRight.Lat(), topRight.Lng())
-	return GetADDSMETARs(url)
+	return GetADDSMETARs(urlLatestADDSDataInRect("metars", bottomLeft, topRight))
 }
 
 // Gets the most recent METARs that were obtained at least within the last 1.5 hours within "radius" of "pt".
 func GetLatestADDSMETARsInRadiusOf(radius uint, pt *geo.Point) ([]ADDSMETAR, error) {
-	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&radialDistance=%d;%f,%f", radius, pt.Lng(), pt.Lat())
-	return GetADDSMETARs(url)
+	return GetADDSMETARs(urlLatestADDSDataInRadiusOf("metars", radius, pt))
 }
 
 // Gets the most recent METARs that were obtained at least within "route" staute miles of the defined route.
 // "route" is in the format "lng1,lat1;lng2,lat2;..." or "ident1;ident2;..."
 func GetLatestADDSMETARsAlongRoute(dist float64, route string) ([]ADDSMETAR, error) {
-	url := fmt.Sprintf("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&flightPath=%f;%s", dist, route)
-	return GetADDSMETARs(url)
+	return GetADDSMETARs(urlLatestADDSDataAlongRoute("metars", dist, route))
 }
 
 func GetLatestADDSMETARs(ident string) (ret ADDSMETAR, err error) {
