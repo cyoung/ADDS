3f8df45908174633a327852c69ceb2ab222788f0
cyoung <cyoung@gmail.com>
2018-12-04 12:13:12 -0500

    Field name change.

--- METAR.go
+++ METAR.go
@@ -5,18 +5,18 @@ import (
 )
 
 type ADDSMETAR struct {
-	Text           string   `xml:"raw_text"`
-	StationID      string   `xml:"station_id"`
-	Observation    ADDSTime `xml:"observation_time"`
-	Latitude       float64  `xml:"latitude"`
-	Longitude      float64  `xml:"longitude"`
-	Temp           float64  `xml:"temp_c"`
-	Dewpoint       float64  `xml:"dewpoint_c"`
-	WindDirection  float64  `xml:"wind_dir_degrees"`
-	WindSpeed      float64  `xml:"wind_speed_kt"`
-	Visibility     float64  `xml:"visibility_statute_mi"`
-	Altimeter      float64  `xml:"altim_in_hg"`
-	FlightCategory string   `xml:"flight_category"`
+	Text            string   `xml:"raw_text"`
+	StationID       string   `xml:"station_id"`
+	ObservationTime ADDSTime `xml:"observation_time"`
+	Latitude        float64  `xml:"latitude"`
+	Longitude       float64  `xml:"longitude"`
+	Temp            float64  `xml:"temp_c"`
+	Dewpoint        float64  `xml:"dewpoint_c"`
+	WindDirection   float64  `xml:"wind_dir_degrees"`
+	WindSpeed       float64  `xml:"wind_speed_kt"`
+	Visibility      float64  `xml:"visibility_statute_mi"`
+	Altimeter       float64  `xml:"altim_in_hg"`
+	FlightCategory  string   `xml:"flight_category"`
 }
 
 func GetADDSMETARs(url string) ([]ADDSMETAR, error) {
